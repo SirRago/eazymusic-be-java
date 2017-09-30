6de0cfa53719e8a400c9e364230254c179a9f9ca
Steven Rago <stevenrago@stevens-mini.attlocal.net>
2017-09-30 12:17:09 -0500

    adding gradle

---
+++ src/main/java/com/eazymusic/eazymusicbejava/EazymusicBeJavaApplication.java
@@ -0,0 +1,12 @@
+package com.eazymusic.eazymusicbejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazymusicBeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazymusicBeJavaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eazymusic/eazymusicbejava/EazymusicBeJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eazymusic.eazymusicbejava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EazymusicBeJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
